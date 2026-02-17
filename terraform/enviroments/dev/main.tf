c1bcf08c6ab4571d36d058ddd02732e150dcd46d
Azeeeeem <calledasazeem@gmail.com>
2026-02-17 14:16:10 +0530

    added vpc in dev

---
+++ terraform/enviroments/dev/main.tf
@@ -0,0 +1,12 @@
+module "vpc" {
+  source          = "../../modules/vpc"
+  vpc_name        = var.vpc_name
+  vpc_cidr        = var.vpc_cidr
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+  azs             = var.azs
+  tags = {
+    Environment = "dev"
+    Project     = "k8s-cluster"
+  }
+}
